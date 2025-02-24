3bbb72e64c864c475b11a5372cb98b13ea98c83b
nathanAndrews03 <andre149@purdue.edu>
2025-02-24 01:41:07 -0500

    added friendship and trip member tables

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -12,15 +12,35 @@ CREATE TABLE users (
      -- updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
+
+-- friendship Table
+CREATE TABLE friendships (
+    user1_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    user2_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    CHECK (user1_id < user2_id)
+);
+
 -- Trips Table
 CREATE TABLE trips (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    creator_id UUID REFERENCES users(id) ON DELETE CASCADE,
     destination VARCHAR(255) NOT NULL,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    is_public BOOLEAN DEFAULT false
     -- this doesn't work in psql need to create  trigger
     -- updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    -- CHECK (start_date <= end_date)  -- Ensures valid trip dates
 );
+
+-- trip_members Table
+CREATE TABLE trip_members (
+    trip_id UUID REFERENCES trips(id) ON DELETE CASCADE,
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    UNIQUE(trip_id, user_id)
+);
+
